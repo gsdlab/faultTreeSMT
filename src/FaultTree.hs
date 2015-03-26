eb0a398bf57f1bace8f41ab263f9addb7b3e09e1
Jordan Ross <j25ross@uwaterloo.ca>
2015-03-26 14:35:33 -0400

    Added another test setup where we query to see if the configuration will satisfy a safety threshold. Also added a probability function that will bascially remove -0.0 from our solutions.

--- src/FaultTree.hs
+++ src/FaultTree.hs
@@ -5,7 +5,15 @@ import Data.SBV
 main :: IO ()
 main = do
     res <- allSat failureExpression
+    --res <- allSat thresholdExpression
     putStrLn $ show res
+
+--CAUTION: Impacts solving time negatively! 
+probability :: String -> Symbolic SDouble
+probability varName = do
+    x <- sDouble varName
+    constrain $ x .>= 0.0 &&& x .<= 1 &&& ((1 / x) .> 0)
+    return x
 
 failureExpression :: Predicate
 failureExpression = do
@@ -20,16 +28,32 @@ failureExpression = do
             sensorFails :: SDouble
             sensorFails = 5e-9
 
-        andFails <- exists "AndFails"
+        andFails <- probability "AndFails"
         constrain $ andFails .== andGate battery batteryBurns sensor sensorFails
 
-        orFails <- exists "OrFails"
+        orFails <- probability "OrFails"
         constrain $ orFails .== orGate battery batteryBurns sensor sensorFails
 
-        failure  <- exists "Failure"
-        constrain $ failure .== batteryBurns
-
         return true
+
+thresholdExpression :: Predicate
+thresholdExpression = do
+    battery <- exists "Battery"
+    let
+        batteryBurns :: SFloat
+        batteryBurns = 1e-9
+
+    sensor <- exists "Sensor"
+    let
+        sensorFails :: SFloat
+        sensorFails = 5e-9
+    orFails <- exists "OrFails"
+    constrain $ orFails .== orGateSingle battery batteryBurns sensor sensorFails
+
+    safetyThreshold <- exists "SafetyThreshold"
+    constrain $ safetyThreshold .== 6e-9
+
+    return $ orFails .< safetyThreshold
 
 andGate :: SBool -> SDouble -> SBool  -> SDouble -> SDouble
 andGate    haveLeft leftProb  haveRight rightProb =
@@ -39,6 +63,18 @@ andGate    haveLeft leftProb  haveRight rightProb =
 
 orGate :: SBool -> SDouble -> SBool  -> SDouble -> SDouble
 orGate    haveLeft leftProb  haveRight rightProb =
+    ite (haveLeft)
+        (ite haveRight (leftProb + rightProb - leftProb * rightProb) leftProb)
+        (ite haveRight rightProb 0)
+
+andGateSingle :: SBool -> SFloat -> SBool  -> SFloat -> SFloat
+andGateSingle    haveLeft leftProb  haveRight rightProb =
+    ite (haveLeft)
+        (ite haveRight (leftProb * rightProb) leftProb)
+        (ite haveRight rightProb 0)
+
+orGateSingle :: SBool -> SFloat -> SBool  -> SFloat -> SFloat
+orGateSingle    haveLeft leftProb  haveRight rightProb =
     ite (haveLeft)
         (ite haveRight (leftProb + rightProb - leftProb * rightProb) leftProb)
         (ite haveRight rightProb 0)
